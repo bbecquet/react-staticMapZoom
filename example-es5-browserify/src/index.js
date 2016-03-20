9c340559794e013a2716b84e5e9648970e835d98
Benjamin Becquet <benjamin.becquet@gmail.com>
2016-03-20 15:51:40 +0100

    Improve examples

--- example-es5-browserify/src/index.js
+++ example-es5-browserify/src/index.js
@@ -4,10 +4,6 @@ var StaticMapZoom = require('../../dist/StaticMapZoom');
 var createMapZoom = React.createFactory(StaticMapZoom);
 
 window.onload = function () {
-    /*ReactDOM.render(createMapZoom({
-        zooms: [1, 10, 20],
-        center: [48.85, 2.35]
-    }), document.getElementById('container'));*/
     ReactDOM.render(createMapZoom({
         zooms: [1, 8, 16],
         center: [48.85, 2.35],
